4075d179b7da77b566f8e8ae55da43e6b546590c
Jawad Aziz Khan <jawad.aziz.khan.jak@gmail.com>
2019-03-31 04:11:21 +0600

    Faster NPY Assisted Dataloader + some changes to the dataset

--- pytorch_datasetloader.py
+++ pytorch_datasetloader.py
@@ -34,37 +34,76 @@ def torchdatagrid(images):
     plt.figure(figsize=(16,8))
     torchimshow(torchvision.utils.make_grid(images))
 
-
-########################################### DATASET ################
-class Picsle8DS(D.Dataset):
-
-    def __init__(self, root):
-        """ Intialize the dataset """
-        self.filearray = []
-        self.labels = []
-        self.root = root
-        self.transform = transforms.ToTensor()
-        filenames = glob.glob(osp.join(self.root+'Pixelart/', '*.jpg'))
-        for fn in filenames:
-            self.filearray.append(fn)
-            self.labels.append(1)
-        filenames = glob.glob(osp.join(self.root+'Realpix/', '*.jpg'))
-        for fn in filenames:
-            self.filearray.append(fn)
-            self.labels.append(0)
-        self.len = len(self.filearray)
-
-    def __getitem__(self, index):
-        """ Get a sample from the dataset """
-        image = Image.open(self.filearray[index])
+def saveimagesasnpy(dir='images/'):
+    filearray = []
+    labels = []
+    filenames = glob.glob(osp.join(dir+'Pixelart/', '*.jpg'))
+    for fn in filenames:
+        filearray.append(fn)
+        labels.append(1)
+        filenames = glob.glob(osp.join(dir+'Realpix/', '*.jpg'))
+    for fn in filenames:
+        filearray.append(fn)
+        labels.append(0)
+    length = len(filearray)
+    imgarr = []
+    for index in range(0,length):
+        image = Image.open(filearray[index])
         if image.size[0] != image.size[1]:
             sqrsize = min(image.size)
             croptrans = transforms.CenterCrop((sqrsize,sqrsize))
             image = croptrans(image)
         nimage = image.resize((192, 192), Image.NEAREST)
         nimage = nimage.convert('RGB')
+        img = np.array(nimage)
+        imgarr.append(img)
+    imgarr = np.array(imgarr)
+    # imgarr = np.moveaxis(imgarr,3,1)
+    np.save('picsle8_ImageArray_774RGB',imgarr)
+    np.save('picsle8_LabelArray_774',labels)
+
+def loadnpyfiles():
+    npzimg = np.load('picsle8_ImageArray_774RGB.npy')
+    npzlbl = np.load('picsle8_LabelArray_774.npy')
+    return npzimg, npzlbl
+
+########################################### DATASET ################
+""" Swap commmented with active code in case of issues """
+class Picsle8DS(D.Dataset):
+
+    def __init__(self, root):
+        """ Intialize the dataset """
+        # self.filearray = []
+        # self.labels = []
+        # self.root = root
+        # self.transform = transforms.ToTensor()
+        # filenames = glob.glob(osp.join(self.root+'Pixelart/', '*.jpg'))
+        # for fn in filenames:
+        #     self.filearray.append(fn)
+        #     self.labels.append(1)
+        # filenames = glob.glob(osp.join(self.root+'Realpix/', '*.jpg'))
+        # for fn in filenames:
+        #     self.filearray.append(fn)
+        #     self.labels.append(0)
+        # self.len = len(self.filearray)
+        self.imgarray = np.load('picsle8_ImageArray_774RGB.npy')
+        self.labels = np.load('picsle8_LabelArray_774.npy')
+        self.len = len(self.imgarray)
+
+    def __getitem__(self, index):
+        """ Get a sample from the dataset """
+        # image = Image.open(self.filearray[index])
+        # if image.size[0] != image.size[1]:
+        #     sqrsize = min(image.size)
+        #     croptrans = transforms.CenterCrop((sqrsize,sqrsize))
+        #     image = croptrans(image)
+        # nimage = image.resize((192, 192), Image.NEAREST)
+        # nimage = nimage.convert('RGB')
+        # return self.transform(nimage), label
+        t = transforms.ToTensor()
+        image = t(self.imgarray[index])
         label = self.labels[index]
-        return self.transform(nimage), label
+        return image, label
 
     def __len__(self):
         """ Total number of samples in the dataset """
